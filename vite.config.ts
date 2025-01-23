5651ff6f358771e8addae3ebfd808509682502e5
李岚清 <958614130@qq.com>
2025-01-23 19:46:04 +0800

    feat(react-tracked): 添加 react-tracked 相关组件和页面
    - 新增 Box、Counter、ReactTracked、Store 和 TextBox 组件
    - 在 routes.tsx 中添加 ReactTracked 页面路由
    - 更新 package.json 和 pnpm-lock.yaml，添加 react-tracked 依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import fs from 'fs';
+import fs from 'node:fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({

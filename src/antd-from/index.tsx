bff6ffe1bdb8c7ec33075dbffcf6dd7bc25acdde
李岚清 <958614130@qq.com>
2025-04-15 20:23:49 +0800

    feat(react-tracked): 添加 react-tracked 相关组件和页面
    - 新增 Box、Counter、ReactTracked、Store 和 TextBox 组件
    - 在 routes.tsx 中添加 ReactTracked 页面路由
    - 更新 package.json 和 pnpm-lock.yaml，添加 react-tracked 依赖

--- src/antd-from/index.tsx
+++ src/antd-from/index.tsx
@@ -21,51 +21,62 @@ const App: React.FC = () => {
   const [form] = Form.useForm();
   useMount(() => {
     form.setFieldsValue({
-      username: '999',
+      // username: '999',
     });
   });
   return (
     <Form
+      rootClassName='asdasd'
       form={form}
       name='basic'
       autoComplete='off'
       labelCol={{ span: 8 }}
       wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
+      style={{
+        width: '50%',
+        border: '1px solid red',
+      }}
       initialValues={{
         remember: true,
-        username: '56165',
+        // username: '56165',
       }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
+      // onValuesChange={(changedValues, allValues) => {
+      //   console.log('===onValuesChange===>', changedValues, allValues);
+      // }}
+      // onFieldsChange={(changedFields, allFields) => {
+      //   console.log('===onFieldsChange===>', changedFields, allFields);
+      // }}
     >
       <Form.Item<FieldType>
         // noStyle
         // hidden
-
         // extra={111}
         // help={999}
+
         // colon={false}
         // label={null}
         // shouldUpdate  //要注意 Form.Item 里包裹的子组件必须由函数返回
         // layout='vertical'
+        // rootClassName='1321'
         labelCol={{
           flex: '0 0 100px',
         }}
         getValueFromEvent={(e) => {
-          // chushi
+          // 只在用户操作有效
           console.log('===getValueFromEvent-1===>', e);
           return e.target.value;
         }}
-        getValueProps={(value) => {
-          //每次初始化或者重新渲染都有效
-          console.log('===getValueProps-2===>', value);
-          return { value };
-        }}
         normalize={(v, pv, s) => {
           // 只在用户操作有效
-          console.log('===normalize-3===>', v);
+          console.log('===normalize-2===>', v);
           return v;
+        }}
+        getValueProps={(value) => {
+          //每次初始化或者重新渲染都有效
+          console.log('===getValueProps-3===>', value);
+          return { value };
         }}
         label='Username'
         name='username'

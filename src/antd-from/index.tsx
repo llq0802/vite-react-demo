f700ea3233d9ebeb831ecec6f275bc15aa138a2f
李岚清 <958614130@qq.com>
2025-09-13 15:05:11 +0800

    refactor(antd-form): 重构表单组件
    - 移除不必要的注释代码
    - 替换 Input 组件为 DatePicker 组件
    - 更新表单样式和布局
    - 优化 getValueFromEvent、normalize 和 getValueProps 的使用
    - 删除未使用的 cmp.less 文件
    - 简化 cmp.tsx 文件

--- src/antd-from/index.tsx
+++ src/antd-from/index.tsx
@@ -1,120 +1,185 @@
+// import React from 'react';
+// import type { FormProps } from 'antd';
+// import { Button, Checkbox, Form, Input } from 'antd';
+// import { useMount } from 'ahooks';
+
+// type FieldType = {
+//   username?: string;
+//   password?: string;
+//   remember?: string;
+// };
+
+// const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+//   console.log('Success:', values);
+// };
+
+// const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+//   console.log('Failed:', errorInfo);
+// };
+
+// const App: React.FC = () => {
+//   const [form] = Form.useForm();
+//   useMount(() => {
+//     form.setFieldsValue({
+//       // username: '999',
+//     });
+//   });
+//   return (
+//     <Form
+//       rootClassName='asdasd'
+//       form={form}
+//       name='basic'
+//       autoComplete='off'
+//       labelCol={{ span: 8 }}
+//       wrapperCol={{ span: 16 }}
+//       style={{
+//         width: '50%',
+//         border: '1px solid red',
+//       }}
+//       initialValues={{
+//         remember: true,
+//         username: '56165',
+//       }}
+//       onFinish={onFinish}
+//       onFinishFailed={onFinishFailed}
+//       // onValuesChange={(changedValues, allValues) => {
+//       //   console.log('===onValuesChange===>', changedValues, allValues);
+//       // }}
+//       // onFieldsChange={(changedFields, allFields) => {
+//       //   console.log('===onFieldsChange===>', changedFields, allFields);
+//       // }}
+//     >
+//       {/* <Form.Item
+//         // noStyle
+//         // hidden
+//         // extra={111}
+//         // help={999}
+
+//         // colon={false}
+//         // label={null}
+//         // shouldUpdate  //要注意 Form.Item 里包裹的子组件必须由函数返回
+//         // layout='vertical'
+//         // rootClassName='1321'
+//         labelCol={{
+//           flex: '0 0 100px',
+//         }}
+//         getValueFromEvent={(e) => {
+//           // 只在用户操作有效
+//           console.log('===getValueFromEvent-1===>', e);
+//           return e.target.value;
+//         }}
+//         normalize={(v, pv, s) => {
+//           // 只在用户操作有效
+//           console.log('===normalize-2===>', v);
+//           return v;
+//         }}
+//         getValueProps={(value) => {
+//           //每次初始化或者重新渲染都有效
+//           console.log('===getValueProps-3===>', value);
+//           return { value };
+//         }}
+//         label='Username'
+//         name='username'
+//         rules={[{ required: true, message: 'Please input your username!' }]}
+//         validateDebounce={200}
+//         // required
+//       >
+//         <Input />
+//       </Form.Item>
+
+//       <Form.Item
+//         required
+//         label='Password'
+//         name='password'
+//         // rules={[{ required: true, message: 'Please input your password!' }]}
+//       >
+//         <Input.Password />
+//       </Form.Item>
+
+//       <Form.Item<FieldType> name='remember' valuePropName='checked' label={null}>
+//         <Checkbox>Remember me</Checkbox>
+//       </Form.Item>
+
+//       <Form.Item label={null}>
+//         <Button type='primary' htmlType='submit'>
+//           Submit
+//         </Button>
+//       </Form.Item> */}
+
+//       <Form.Item required name={['k', '0']}>
+//         <Input />
+//       </Form.Item>
+//       <Form.Item required name={['k', '1']}>
+//         <Input />
+//       </Form.Item>
+//       <Form.Item required name={[1, 'username2']}>
+//         <Input />
+//       </Form.Item>
+//       <Form.Item label={null}>
+//         <Button type='primary' htmlType='submit'>
+//           Submit
+//         </Button>
+//       </Form.Item>
+//     </Form>
+//   );
+// };
+
+// export default App;
+
 import React from 'react';
 import type { FormProps } from 'antd';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, DatePicker, Form } from 'antd';
+import dayjs from 'dayjs';
 import { useMount } from 'ahooks';
 
+const dateTimestamp = dayjs('2024-01-01').valueOf();
+
 type FieldType = {
-  username?: string;
-  password?: string;
-  remember?: string;
+  date?: string;
 };
 
 const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
   console.log('Success:', values);
 };
 
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
-
 const App: React.FC = () => {
   const [form] = Form.useForm();
   useMount(() => {
     form.setFieldsValue({
-      // username: '999',
+      date: dateTimestamp,
     });
   });
   return (
     <Form
-      rootClassName='asdasd'
       form={form}
-      name='basic'
-      autoComplete='off'
+      name='getValueProps'
       labelCol={{ span: 8 }}
       wrapperCol={{ span: 16 }}
-      style={{
-        width: '50%',
-        border: '1px solid red',
-      }}
-      initialValues={{
-        remember: true,
-        username: '56165',
-      }}
+      style={{ maxWidth: 600 }}
+      // initialValues={{ date: dateTimestamp }}
       onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      // onValuesChange={(changedValues, allValues) => {
-      //   console.log('===onValuesChange===>', changedValues, allValues);
-      // }}
-      // onFieldsChange={(changedFields, allFields) => {
-      //   console.log('===onFieldsChange===>', changedFields, allFields);
-      // }}
+      autoComplete='off'
     >
-      {/* <Form.Item
-        // noStyle
-        // hidden
-        // extra={111}
-        // help={999}
-
-        // colon={false}
-        // label={null}
-        // shouldUpdate  //要注意 Form.Item 里包裹的子组件必须由函数返回
-        // layout='vertical'
-        // rootClassName='1321'
-        labelCol={{
-          flex: '0 0 100px',
+      <Form.Item<FieldType>
+        label='Date'
+        name='date'
+        rules={[{ required: true }]}
+        getValueFromEvent={(value) => {
+          console.log('===value-getValueFromEvent===>', value);
+          return value && value.valueOf();
         }}
-        getValueFromEvent={(e) => {
-          // 只在用户操作有效
-          console.log('===getValueFromEvent-1===>', e);
-          return e.target.value;
-        }}
-        normalize={(v, pv, s) => {
-          // 只在用户操作有效
-          console.log('===normalize-2===>', v);
-          return v;
+        normalize={(value) => {
+          console.log('===value-normalize===>', value);
+          return value && dayjs(value).valueOf();
         }}
         getValueProps={(value) => {
-          //每次初始化或者重新渲染都有效
-          console.log('===getValueProps-3===>', value);
-          return { value };
+          console.log('===value-getValueProps===>', value);
+          return { value: value && dayjs(Number(value)) };
         }}
-        label='Username'
-        name='username'
-        rules={[{ required: true, message: 'Please input your username!' }]}
-        validateDebounce={200}
-        // required
       >
-        <Input />
+        <DatePicker />
       </Form.Item>
 
-      <Form.Item
-        required
-        label='Password'
-        name='password'
-        // rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item<FieldType> name='remember' valuePropName='checked' label={null}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item label={null}>
-        <Button type='primary' htmlType='submit'>
-          Submit
-        </Button>
-      </Form.Item> */}
-
-      <Form.Item required name={['k', 0]}>
-        <Input />
-      </Form.Item>
-      <Form.Item required name={['k', '1']}>
-        <Input />
-      </Form.Item>
-      <Form.Item required name={[1, 'username2']}>
-        <Input />
-      </Form.Item>
       <Form.Item label={null}>
         <Button type='primary' htmlType='submit'>
           Submit
@@ -125,61 +190,6 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-// import React from 'react';
-// import type { FormProps } from 'antd';
-// import { Button, DatePicker, Form } from 'antd';
-// import dayjs from 'dayjs';
-
-// const dateTimestamp = dayjs('2024-01-01').valueOf();
-
-// type FieldType = {
-//   date?: string;
-// };
-
-// const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-//   console.log('Success:', values);
-// };
-
-// const App: React.FC = () => (
-//   <Form
-//     name='getValueProps'
-//     labelCol={{ span: 8 }}
-//     wrapperCol={{ span: 16 }}
-//     style={{ maxWidth: 600 }}
-//     initialValues={{ date: dateTimestamp }}
-//     onFinish={onFinish}
-//     autoComplete='off'
-//   >
-//     <Form.Item<FieldType>
-//       label='Date'
-//       name='date'
-//       rules={[{ required: true }]}
-//       getValueFromEvent={(value) => {
-//         console.log('===value-getValueFromEvent===>', value);
-//         return value && value.valueOf();
-//       }}
-//       normalize={(value) => {
-//         console.log('===value-normalize===>', value);
-//         return value && dayjs(value).valueOf();
-//       }}
-//       getValueProps={(value) => {
-//         console.log('===value-getValueProps===>', value);
-//         return { value: value && dayjs(Number(value)) };
-//       }}
-//     >
-//       <DatePicker />
-//     </Form.Item>
-
-//     <Form.Item label={null}>
-//       <Button type='primary' htmlType='submit'>
-//         Submit
-//       </Button>
-//     </Form.Item>
-//   </Form>
-// );
-
-// export default App;
 
 // function fn<T>(parameter:T) {
 //   return parameter
--- src/home/cmp.tsx
+++ src/home/cmp.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import styles from './cmp.less';
 
 type PropsType = {};
 
 const Cmp: React.FC<PropsType> = ({}) => {
-  return <div className={styles.container}>Cmp</div>;
+  return <div>Cmp</div>;
 };
 
 export default Cmp;

d456ededd2327580303561b800231514b2675e85
李岚清 <958614130@qq.com>
2025-09-24 20:21:20 +0800

    build: 升级 Vite 版本至 7.0.0
    - 将 package.json 中的 "vite" 依赖版本从 5.2.0 升级到 7.0.0

--- src/penpal/index.tsx
+++ src/penpal/index.tsx
@@ -1,43 +1,78 @@
-import { useEffect, useState } from 'react';
-import { PenpalParent } from '@weblivion/react-penpal';
-import type { RemoteProxy, Reply } from 'penpal';
+// import { useEffect, useState } from 'react';
+// import { PenpalParent } from '@weblivion/react-penpal';
+// import type { RemoteProxy, Reply } from 'penpal';
 
-type ChildMethods = {
-  hi: (s: string) => Reply<string>;
+// type ChildMethods = {
+//   hi: (s: string) => Reply<string>;
+// };
+
+// function Penpal() {
+//   const [child, setChild] = useState<RemoteProxy<ChildMethods>>(null!);
+//   const [message, setMessage] = useState('');
+
+//   useEffect(() => {
+//     console.log('===child==>', child);
+//     if (child) {
+//       child.hi('Hi from PenpalParent');
+//     }
+//   }, [child]);
+
+//   return (
+//     <div style={{ width: '100%', height: '100%' }}>
+//       <h1>child.hello(): {message}</h1>
+//       <PenpalParent
+//         src='http://localhost:8000/components/l-captcha-button'
+//         width='100%'
+//         height='100%'
+//         setChild={setChild}
+//         allowedOrigins={['*']}
+//         methods={{
+//           hello(message: string) {
+//             setMessage(message);
+//           },
+//         }}
+//         style={{
+//           border: '0',
+//           display: 'block',
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Penpal;
+
+import { usePenpalParent } from '@weblivion/react-penpal';
+import type { Reply } from 'penpal';
+import { useEffect, useState } from 'react';
+
+type ParentMethods = {
+  hello(msg: string): Reply<string>;
 };
 
 function Penpal() {
-  const [child, setChild] = useState<RemoteProxy<ChildMethods>>(null!);
   const [message, setMessage] = useState('');
+  const { parentMethods, connection } = usePenpalParent<ParentMethods>(
+    {
+      methods: {
+        hi(message: string) {
+          setMessage(message);
+        },
+      },
+    },
+    ['*'],
+  );
 
   useEffect(() => {
-    console.log('===child==>', child);
-    if (child) {
-      child.hi('Hi from PenpalParent');
+    if (connection) {
+      // parentMethods.hello('Hello from usePenpalParent');
     }
-  }, [child]);
+  }, [connection, parentMethods]);
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
-      <h1>child.hello(): {message}</h1>
-      <PenpalParent
-        src='http://localhost:8000/components/l-captcha-button'
-        width='100%'
-        height='100%'
-        setChild={setChild}
-        allowedOrigins={['*']}
-        methods={{
-          hello(message: string) {
-            setMessage(message);
-          },
-        }}
-        style={{
-          border: '0',
-          display: 'block',
-        }}
-      />
+    <div>
+      <h1>parent.hi(): {message}</h1>
     </div>
   );
 }
-
 export default Penpal;

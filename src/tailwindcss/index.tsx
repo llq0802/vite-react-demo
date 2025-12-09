0d05225cef5a19ef02c36a74a39bd0a104527ec3
李岚清 <958614130@qq.com>
2025-12-09 16:29:40 +0800

    ``` feat(tailwindcss): 更新样式配置与类名优化
    调整 HTML 结构与 CSS 样式，引入 TailwindCSS 的 reference 指令，
    并优化容器及元素的布局与字体大小。移除冗余样式定义，
    统一通过 Tailwind 工具类控制显示效果，并新增一个禁用拼写检查的输入框。
    ```

--- src/tailwindcss/index.tsx
+++ src/tailwindcss/index.tsx
@@ -2,16 +2,17 @@ import { cn } from '../utils';
 import styles from './index.module.css';
 
 const Tailwindcss = () => {
-  const item = `flex justify-center items-center w-full h-full border`;
+  const item = `center w-full h-full border bg-sky-300`;
   return (
     <div className={cn(styles.container, 'content-auto')}>
       <div className="flex items-center justify-center gap-2">
         <div className="bg-sky-500 p-3 hover:bg-sky-700">Save changes</div>
         <div className="center h-full w-1/2 bg-sky-500 text-base font-bold text-amber-400">测试</div>
+        <input type="text" spellCheck="false" className="" />
       </div>
 
       <div className="grid! h-full grid-cols-2 gap-2 sm:grid-cols-3">
-        <div className={cn(item)}>1</div>
+        <div className={cn(item, 'bg-amber-100')}>1</div>
         <div className={cn(item)}>2</div>
         <div className={cn(item)}>3</div>
         <div className={cn(item)}>4</div>
